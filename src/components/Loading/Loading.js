d2a251617a90216934825c26c732263e44ea5f77
Mike Plis <michael.plis01@gmail.com>
2017-12-09 12:31:58 -0500

    fix loader

--- src/components/Loading/Loading.js
+++ src/components/Loading/Loading.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import { RingLoader } from 'react-spinners';
+import { PulseLoader } from 'react-spinners';
 
 import './Loading.css';
 
 export default props => {
     return (
         <div className="loader">
-            <RingLoader size={200} color="#bada55" />
+            <PulseLoader size={50} color="#bada55" />
         </div>
     );
 };
