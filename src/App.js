39972b5f78fb557bf6a29af7df5ecd5f3a15863a
Michael DePetrillo <michael.depetrillo@gmail.com>
2018-12-01 09:08:04 -0500

    Update playoff tracker data source

--- src/App.js
+++ src/App.js
@@ -12,89 +12,112 @@ import './App.css';
 const franchise = new schema.Entity('franchises');
 
 const projectedScore = new schema.Entity(
-    'projectedScores',
-    {},
-    {
-        processStrategy: projectedScore => ({
-            ...projectedScore,
-            score: Number.parseFloat(projectedScore.score) || 0
-        })
-    }
+  'projectedScores',
+  {},
+  {
+    processStrategy: projectedScore => ({
+      ...projectedScore,
+      score: Number.parseFloat(projectedScore.score) || 0,
+    }),
+  }
 );
 
 const liveScore = new schema.Entity(
-    'liveScores',
-    {},
-    {
-        processStrategy: liveScore => ({
-            ...liveScore,
-            score: Number.parseFloat(liveScore.score),
-            gameSecondsRemaining: Number.parseInt(liveScore.gameSecondsRemaining, 10),
-            playersYetToPlay: Number.parseInt(liveScore.playersYetToPlay, 10)
-        })
-    }
+  'liveScores',
+  {},
+  {
+    processStrategy: liveScore => ({
+      ...liveScore,
+      score: Number.parseFloat(liveScore.score),
+      gameSecondsRemaining: Number.parseInt(liveScore.gameSecondsRemaining, 10),
+      playersYetToPlay: Number.parseInt(liveScore.playersYetToPlay, 10),
+    }),
+  }
 );
 
 const player = new schema.Entity('players');
 const division = new schema.Entity('divisions');
 
 const dataSchema = new schema.Object({
+  league: {
     league: {
-        league: {
-            franchises: {
-                franchise: new schema.Array(franchise)
-            },
-            divisions: {
-                division: new schema.Array(division)
-            }
-        }
+      franchises: {
+        franchise: new schema.Array(franchise),
+      },
+      divisions: {
+        division: new schema.Array(division),
+      },
     },
-    liveScores: {
-        liveScoring: {
-            matchup: new schema.Array({
-                franchise: new schema.Array(liveScore)
-            }),
-            franchise: new schema.Array(liveScore)
-        }
+  },
+  liveScores: {
+    liveScoring: {
+      matchup: new schema.Array({
+        franchise: new schema.Array(liveScore),
+      }),
+      franchise: new schema.Array(liveScore),
     },
+  },
+  projectedScores: {
     projectedScores: {
-        projectedScores: {
-            playerScore: new schema.Array(projectedScore)
-        }
+      playerScore: new schema.Array(projectedScore),
     },
+  },
+  players: {
     players: {
-        players: {
-            player: new schema.Array(player)
-        }
-    }
+      player: new schema.Array(player),
+    },
+  },
 });
 
 class App extends Component {
-    state = {
-        entities: {},
-        data: {}
-    };
+  state = {
+    entities: {},
+    data: {},
+  };
 
-    componentDidMount() {
-        axios.get('https://mfl-api.herokuapp.com/').then(({ data }) => {
-            const { entities, result } = normalize(data, dataSchema);
-            this.setState({
-                entities,
-                data: result
-            });
-        });
-    }
+  componentDidMount() {
+    const LeagueID = 10507;
+    const Year = 2018;
+    const Week = 13;
+    const apiUrl =
+      'https://cors-anywhere.herokuapp.com/https://api.myfantasyleague.com/' +
+      Year +
+      '/export?JSON=1';
+    const LeagueIDParam = '&L=' + LeagueID;
+    const WeekParam = '&W=' + Week;
 
-    render() {
-        return (
-            <BrowserRouter basename="/playoff-tracker/">
-                <div className="app">
-                    <Route exact path="/" render={() => <ResultsTable {...this.state} />} />
-                    <Route path="/admin" render={() => <Admin {...this.state} />} />
-                </div>
-            </BrowserRouter>
-        );
-    }
+    const PlayersUrl = apiUrl + '&TYPE=players';
+    const LeagueUrl = apiUrl + '&TYPE=league' + LeagueIDParam;
+    const ProjectedScoresUrl =
+      apiUrl + '&TYPE=projectedScores' + LeagueIDParam + WeekParam + '&COUNT=99999';
+    const LiveScoringUrl = apiUrl + '&TYPE=liveScoring' + LeagueIDParam + WeekParam;
+
+    var rootJsonData = {};
+
+    Promise.all([
+      axios.get(PlayersUrl).then(({ data }) => (rootJsonData.players = data)),
+      axios.get(LeagueUrl).then(({ data }) => (rootJsonData.league = data)),
+      axios.get(ProjectedScoresUrl).then(({ data }) => (rootJsonData.projectedScores = data)),
+      axios.get(LiveScoringUrl).then(({ data }) => (rootJsonData.liveScores = data)),
+    ]).then(() => {
+      const { entities, result } = normalize(rootJsonData, dataSchema);
+      this.setState({
+        entities,
+        data: result,
+      });
+    });
+  }
+
+  render() {
+    return (
+      <BrowserRouter basename="/playoff-tracker/">
+        <div className="app">
+          <Route exact path="/" render={() => <ResultsTable {...this.state} />} />
+          <Route path="/admin" render={() => <Admin {...this.state} />} />
+        </div>
+      </BrowserRouter>
+    );
+  }
 }
 
 export default App;
